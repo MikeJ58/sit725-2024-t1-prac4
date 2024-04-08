45c6f21774b3e9f1a744b62ba6d3220f9a3a09f9
MikeJ58 <jmic@deakin.edu.au>
2024-04-08 20:25:23 +1000

    Commit

--- public/server.js
+++ public/server.js
@@ -24,3 +24,37 @@ var port = process.env.port || 3000;
 app.listen(port, () => {
   console.log("App listening to: " + port)
 })
+
+// Require the MongoDB client
+const MongoClient = require('mongodb').MongoClient;
+
+// Connection URL and database name
+const url = 'mongodb://localhost:3000';
+const dbName = 'mydatabase';
+
+// Connect to the MongoDB server
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+    if(err) {
+        console.error('Error occurred while connecting to MongoDB:', err);
+        return;
+    }
+    console.log('Connected successfully to server');
+
+    const db = client.db(dbName);
+
+    // Get the collection
+    const collection = db.collection('mycollection');
+
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+        if(err) {
+            console.error('Error occurred while fetching documents:', err);
+            return;
+        }
+        console.log('Found the following documents:');
+        console.log(docs);
+        
+        // Close the client connection
+        client.close();
+    });
+});
