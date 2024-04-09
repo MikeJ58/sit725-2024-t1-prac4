bec690575dc9f9ff1945378cdce0f519b8d205af
MikeJ58 <jmic@deakin.edu.au>
2024-04-09 18:33:56 +1000

    Commit

--- public/scripts.js
+++ public/scripts.js
@@ -42,42 +42,48 @@ const addCards = (items) => {
 };
 
 const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!");
+};
 
-    alert("Thanks for clicking me. Hope you have a nice day!")
-}
 const submitForm = () => {
     let formData = {};
     formData.title = $('#title').val();
     formData.color = $('#Color').val();
     formData.path = $('#Path').val();
-    formData.desciption = $('#Description').val();
+    formData.description = $('#Description').val();
     console.log("Form Data Submitted: ", formData);
-}
-const addCards = (items) => {
-    items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-            '<div class="card medium"> <div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">'+
-            '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
-            '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text">' + item.desciption + '</p>' +
-            '</div></div></div>';
-        $("#card-section").append(itemToAppend)
-    });
-}
+};
+
+$('#clickMeButton').click(function() {
+    $('#formSection').toggle(); // Toggle the visibility of the form
+});
+
+$(document).ready(function () {
+    $('.modal').modal(); // Initialize modal
+});
+
+
+$('#carForm').submit(function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    $('#modalForm').modal('close'); // Close the modal
+    
+    // Call your function to handle form submission
+    submitForm();
+});
+
 const getProjects = () => {
-    $.get('/api/projects',(response) => {
-    if(response.statusCode==200){
-    addCards(response.data);
-    }
-    })
-    }    
+    $.get('/api/projects', (response) => {
+        if (response.statusCode == 200) {
+            addCards(response.data);
+        }
+    });
+};
+
 $(document).ready(function () {
     $('.materialboxed').materialbox();
     $('#formSubmit').click(() => {
         submitForm();
-    })
+    });
     addCards(cardList);
     $('.modal').modal();
 });
