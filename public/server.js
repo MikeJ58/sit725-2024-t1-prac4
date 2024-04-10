dbf766522062cfa3b9dd84fa7719a280f7b7db7a
MikeJ58 <jmic@deakin.edu.au>
2024-04-10 15:24:44 +1000

    Commit

--- public/server.js
+++ public/server.js
@@ -7,7 +7,7 @@ let projectCollection;
 
 // Database Connection
 
-const uri = "mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PASSWORD+"@cluster1.fopezw4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1" // replace it with the url you get from mongo atlas
+const uri = "mongodb+srv://"+process.env.dbUser+":"+process.env.SimplePlan89+"@cluster1.fopezw4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1" // replace it with the url you get from mongo atlas
 const client = new MongoClient(uri,{ useNewUrlParser: true })
 
 
@@ -73,19 +73,6 @@ app.post('/api/projects',(req,res) => {
             res.json({statusCode: 200, message:"Project Successfully added", data: result})
         }
     })
-})
-const addNumbers = (number1, number2) => {
-    var num1 = parseInt(number1)
-    var num2 = parseInt(number2)
-    var result = num1 + num2;
-    return result;
-}
-
-app.get("/addTwoNumbers",(req,res) => {
-    var number1 = req.query.number1;
-    var number2 = req.query.number2;
-    var result = addNumbers(number1,number2)
-    res.json({statusCode: 200, data: result, message:"Success"})
 })
 
 var port = process.env.port || 3000;
